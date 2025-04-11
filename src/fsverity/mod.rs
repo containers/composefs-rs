fbd30e0b3f0e87f905c2171d81009fa360aa1834
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2025-04-11 11:24:08 +0200

    src/fsverity: add some better tests
    Add a unit test which checks our userspace SHA-256 and SHA-512 fs-verity
    digest calculations of various interesting sizes against the values that
    the kernel computes.
    
    Add a bunch of other tests to verify the correct operation of our
    various ioctl wrappers under success and failure conditions.
    
    Signed-off-by: Allison Karlitskaya <allison.karlitskaya@redhat.com>

--- src/fsverity/mod.rs
+++ src/fsverity/mod.rs
@@ -164,19 +164,211 @@ pub fn ensure_verity_equal(
 
 #[cfg(test)]
 mod tests {
+    use std::{collections::BTreeSet, io::Write};
+
+    use crate::{
+        test::tempfile,
+        util::{parse_sha256, proc_self_fd},
+    };
+    use rustix::{
+        fd::OwnedFd,
+        fs::{open, Mode, OFlags},
+    };
+    use tempfile::tempfile_in;
+
     use super::*;
+
+    fn rdonly_file_with(data: &[u8]) -> OwnedFd {
+        let mut file = tempfile();
+        file.write_all(data).unwrap();
+        let fd = open(
+            proc_self_fd(&file),
+            OFlags::RDONLY | OFlags::CLOEXEC,
+            Mode::empty(),
+        )
+        .unwrap();
+        drop(file); // can't enable verity with outstanding writable fds
+        fd
+    }
 
     #[test]
     fn test_verity_missing() {
-        let tf = tempfile::tempfile().unwrap();
-        match measure_verity::<Sha256HashValue>(&tf) {
-            Err(MeasureVerityError::VerityMissing) => {}
-            o => panic!("Unexpected {o:?}"),
+        let tf = rdonly_file_with(b"");
+
+        assert!(matches!(
+            measure_verity::<Sha256HashValue>(&tf).unwrap_err(),
+            MeasureVerityError::VerityMissing
+        ));
+
+        assert!(measure_verity_opt::<Sha256HashValue>(&tf)
+            .unwrap()
+            .is_none());
+
+        assert!(matches!(
+            ensure_verity_equal(&tf, &Sha256HashValue::EMPTY).unwrap_err(),
+            CompareVerityError::Measure(MeasureVerityError::VerityMissing)
+        ));
+    }
+
+    #[test]
+    fn test_verity_simple() {
+        let tf = rdonly_file_with(b"hello world");
+
+        // first time: success
+        enable_verity::<Sha256HashValue>(&tf).unwrap();
+
+        // second time: fail with "already enabled"
+        assert!(matches!(
+            enable_verity::<Sha256HashValue>(&tf).unwrap_err(),
+            EnableVerityError::AlreadyEnabled
+        ));
+
+        assert_eq!(
+            hex::encode(measure_verity::<Sha256HashValue>(&tf).unwrap()),
+            "1e2eaa4202d750a41174ee454970b92c1bc2f925b1e35076d8c7d5f56362ba64"
+        );
+
+        assert_eq!(
+            hex::encode(measure_verity_opt::<Sha256HashValue>(&tf).unwrap().unwrap()),
+            "1e2eaa4202d750a41174ee454970b92c1bc2f925b1e35076d8c7d5f56362ba64"
+        );
+
+        ensure_verity_equal(
+            &tf,
+            &parse_sha256("1e2eaa4202d750a41174ee454970b92c1bc2f925b1e35076d8c7d5f56362ba64")
+                .unwrap(),
+        )
+        .unwrap();
+
+        let Err(CompareVerityError::DigestMismatch { expected, found }) = ensure_verity_equal(
+            &tf,
+            &parse_sha256("1e2eaa4202d750a41174ee454970b92c1bc2f925b1e35076d8c7000000000000")
+                .unwrap(),
+        ) else {
+            panic!("Didn't fail with expected error");
+        };
+        assert_eq!(
+            expected,
+            "1e2eaa4202d750a41174ee454970b92c1bc2f925b1e35076d8c7000000000000"
+        );
+        assert_eq!(
+            found,
+            "1e2eaa4202d750a41174ee454970b92c1bc2f925b1e35076d8c7d5f56362ba64"
+        );
+    }
+
+    #[test_with::path(/dev/shm)]
+    #[test]
+    fn test_verity_error_noverity() {
+        let tf = tempfile_in("/dev/shm").unwrap();
+
+        assert!(matches!(
+            enable_verity::<Sha256HashValue>(&tf).unwrap_err(),
+            EnableVerityError::FilesystemNotSupported
+        ));
+
+        assert!(matches!(
+            measure_verity::<Sha256HashValue>(&tf).unwrap_err(),
+            MeasureVerityError::VerityMissing
+        ));
+
+        assert!(measure_verity_opt::<Sha256HashValue>(&tf)
+            .unwrap()
+            .is_none());
+
+        assert!(matches!(
+            ensure_verity_equal(&tf, &Sha256HashValue::EMPTY).unwrap_err(),
+            CompareVerityError::Measure(MeasureVerityError::VerityMissing)
+        ));
+    }
+
+    #[test]
+    fn test_verity_wrongdigest_sha512_sha256() {
+        let tf = rdonly_file_with(b"hello world");
+
+        // Enable with SHA-512 but then try to read with SHA-256
+        enable_verity::<Sha512HashValue>(&tf).unwrap();
+
+        assert!(matches!(
+            measure_verity::<Sha256HashValue>(&tf).unwrap_err(),
+            MeasureVerityError::InvalidDigestSize { .. }
+        ));
+
+        assert!(matches!(
+            measure_verity_opt::<Sha256HashValue>(&tf).unwrap_err(),
+            MeasureVerityError::InvalidDigestSize { .. }
+        ));
+
+        assert!(matches!(
+            ensure_verity_equal(&tf, &Sha256HashValue::EMPTY).unwrap_err(),
+            CompareVerityError::Measure(MeasureVerityError::InvalidDigestSize { .. })
+        ));
+    }
+
+    #[test]
+    fn test_verity_wrongdigest_sha256_sha512() {
+        let tf = rdonly_file_with(b"hello world");
+
+        // Enable with SHA-256 but then try to read with SHA-512
+        enable_verity::<Sha256HashValue>(&tf).unwrap();
+
+        assert!(matches!(
+            measure_verity::<Sha512HashValue>(&tf).unwrap_err(),
+            MeasureVerityError::InvalidDigestAlgorithm { .. }
+        ));
+
+        assert!(matches!(
+            measure_verity_opt::<Sha512HashValue>(&tf).unwrap_err(),
+            MeasureVerityError::InvalidDigestAlgorithm { .. }
+        ));
+
+        assert!(matches!(
+            ensure_verity_equal(&tf, &Sha512HashValue::EMPTY).unwrap_err(),
+            CompareVerityError::Measure(MeasureVerityError::InvalidDigestAlgorithm { .. })
+        ));
+    }
+
+    #[test]
+    fn crosscheck_interesting_cases() {
+        // Test the kernel against our userspace calculations.
+        //
+        // We try to pick some "interesting" sizes to test the edge cases.  The arity of a
+        // SHA-256/4096 Merkle tree is 128 = 4096 / 32.  With SHA-512 it's 64 = 4096 / 64.
+        // So we try to chose values around the page size times powers of 32 and 64.
+        let mut cases = BTreeSet::new();
+        for arity in [32, 64] {
+            for layer4 in [/* -1, */ 0 /*, 1 */] {
+                /* otherwise it's too slow */
+                for layer3 in [-1, 0, 1] {
+                    for layer2 in [-1, 0, 1] {
+                        for layer1 in [-1, 0, 1] {
+                            for layer0 in [-1, 0, 1] {
+                                let candidate = layer4 * (arity * arity * arity * arity)
+                                    + layer3 * (arity * arity * arity)
+                                    + layer2 * (arity * arity)
+                                    + layer1 * arity
+                                    + layer0;
+                                if let Ok(size) = usize::try_from(candidate) {
+                                    cases.insert(size);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-        let h = Sha256HashValue::default();
-        match ensure_verity_equal(&tf, &h) {
-            Err(CompareVerityError::Measure(MeasureVerityError::VerityMissing)) => {}
-            o => panic!("Unexpected {o:?}"),
+
+        fn assert_kernel_equal<H: FsVerityHashValue>(data: &[u8], expected: H) {
+            let fd = rdonly_file_with(data);
+            enable_verity::<H>(&fd).unwrap();
+            ensure_verity_equal(&fd, &expected).unwrap();
+        }
+
+        for size in cases {
+            // the actual data is uninteresting
+            let data = vec![0x5a; size];
+            assert_kernel_equal(&data, compute_verity::<Sha256HashValue>(&data));
+            assert_kernel_equal(&data, compute_verity::<Sha512HashValue>(&data));
         }
     }
 }
