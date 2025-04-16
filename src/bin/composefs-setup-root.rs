49ba9ddb950332e9b8537391453a6e7c9ee4933c
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2025-04-16 16:05:38 +0200

    src: use `#[cfg(test)] mod test { }` consistently
    There are a few cases left over from before I learned how to do this
    properly.  Fix them.
    
    Signed-off-by: Allison Karlitskaya <allison.karlitskaya@redhat.com>

--- src/bin/composefs-setup-root.rs
+++ src/bin/composefs-setup-root.rs
@@ -284,18 +284,23 @@ fn main() -> Result<()> {
     setup_root(args)
 }
 
-#[test]
-fn test_parse() {
-    let failing = ["", "foo", "composefs", "composefs=foo"];
-    for case in failing {
-        assert!(parse_composefs_cmdline(case.as_bytes()).is_err());
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let failing = ["", "foo", "composefs", "composefs=foo"];
+        for case in failing {
+            assert!(parse_composefs_cmdline(case.as_bytes()).is_err());
+        }
+        let digest = "8b7df143d91c716ecfa5fc1730022f6b421b05cedee8fd52b1fc65a96030ad52";
+        let digest_bytes = hex::decode(digest).unwrap();
+        similar_asserts::assert_eq!(
+            parse_composefs_cmdline(format!("composefs={digest}").as_bytes())
+                .unwrap()
+                .as_slice(),
+            &digest_bytes
+        );
     }
-    let digest = "8b7df143d91c716ecfa5fc1730022f6b421b05cedee8fd52b1fc65a96030ad52";
-    let digest_bytes = hex::decode(digest).unwrap();
-    similar_asserts::assert_eq!(
-        parse_composefs_cmdline(format!("composefs={digest}").as_bytes())
-            .unwrap()
-            .as_slice(),
-        &digest_bytes
-    );
 }
