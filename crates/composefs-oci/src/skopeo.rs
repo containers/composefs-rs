containers/composefs-rs

commit 54c3c6ded5a537c89751d98db0eb9696cd5a703e

splitstream: Rework file format
This is a substantial change to the splitstream file format to add more
features (required for ostree support) and to add forwards- and
backwards- compatibility mechanisms for future changes.  This change
aims to finalize the file format so we can start shipping this to the
systems of real users without future "breaks everything" changes.

This change itself breaks everything: you'll need to delete your
repository and start over.  Hopefully this is the last time.

The file format is substantially more fleshed-out at this point.  Here's
an overview of the changes:

  - there is a header with a magic value, a version. flags field, and the
    fs-verity algorithm number and block size in use

  - everything else in the file can be freely located which will help if
    we ever want to create a version of the writer that streams data to
    disk as it goes: in that case we may want to store the stream before
    the associated metadata

  - there is an expandable "info" section which contains most other
    information about the stream and is intended to be used as the primary
    mechanism for making compatible changes to the file format in the
    future

  - the info section stores the total decompressed/reassembled stream
    size and a unique identifier value for the file type stored in the
    stream

  - the referenced external objects and splitstreams are now stored in a
    flat array of binary fs-verity hash values to improve the performance
    of garbage collection operations in large repositories (informed by
    Alex's battlescars from dealing with GC on Flathub)

  - it is possible to add arbitrary external object and stream references

  - the "sha256 mapping" has been replaced with a more flexible "named
    stream refs" mechanism that allows assigning arbitrary names to
    associated streams.  This will be useful if we ever want to support
    formats that are based on anything other than SHA-256 (including

crates/composefs-oci/src/skopeo.rs
@@ -20,11 +20,13 @@ use oci_spec::image::{Descriptor, ImageConfiguration, ImageManifest, MediaType};
 use rustix::process::geteuid;
 use tokio::{io::AsyncReadExt, sync::Semaphore};
 
-use composefs::{
-    fsverity::FsVerityHashValue, repository::Repository, splitstream::DigestMap, util::Sha256Digest,
-};
+use composefs::{fsverity::FsVerityHashValue, repository::Repository};
 
-use crate::{sha256_from_descriptor, sha256_from_digest, tar::split_async, ContentAndVerity};
+use crate::{config_identifier, layer_identifier, tar::split_async, ContentAndVerity};
+
+// Content type identifiers stored as ASCII in the splitstream file
+pub(crate) const TAR_LAYER_CONTENT_TYPE: u64 = u64::from_le_bytes(*b"ocilayer");
+pub(crate) const OCI_CONFIG_CONTENT_TYPE: u64 = u64::from_le_bytes(*b"ociconfg");
 
 struct ImageOp<ObjectID: FsVerityHashValue> {
     repo: Arc<Repository<ObjectID>>,
@@ -84,18 +86,15 @@ impl<ObjectID: FsVerityHashValue> ImageOp<ObjectID> {
         })
     }
 
-    pub async fn ensure_layer(
-        &self,
-        layer_sha256: Sha256Digest,
-        descriptor: &Descriptor,
-    ) -> Result<ObjectID> {
+    pub async fn ensure_layer(&self, diff_id: &str, descriptor: &Descriptor) -> Result<ObjectID> {
         // We need to use the per_manifest descriptor to download the compressed layer but it gets
         // stored in the repository via the per_config descriptor.  Our return value is the
         // fsverity digest for the corresponding splitstream.
+        let content_id = layer_identifier(diff_id);
 
-        if let Some(layer_id) = self.repo.check_stream(&layer_sha256)? {
+        if let Some(layer_id) = self.repo.has_stream(&content_id)? {
             self.progress
-                .println(format!("Already have layer {}", hex::encode(layer_sha256)))?;
+                .println(format!("Already have layer {diff_id}"))?;
             Ok(layer_id)
         } else {
             // Otherwise, we need to fetch it...
@@ -109,10 +108,9 @@ impl<ObjectID: FsVerityHashValue> ImageOp<ObjectID> {
                 .unwrap()
                 .progress_chars("##-"));
             let progress = bar.wrap_async_read(blob_reader);
-            self.progress
-                .println(format!("Fetching layer {}", hex::encode(layer_sha256)))?;
+            self.progress.println(format!("Fetching layer {diff_id}"))?;
 
-            let mut splitstream = self.repo.create_stream(Some(layer_sha256), None);
+            let mut splitstream = self.repo.create_stream(TAR_LAYER_CONTENT_TYPE);
             match descriptor.media_type() {
                 MediaType::ImageLayer => {
                     split_async(progress, &mut splitstream).await?;
@@ -125,15 +123,13 @@ impl<ObjectID: FsVerityHashValue> ImageOp<ObjectID> {
                 }
                 other => bail!("Unsupported layer media type {other:?}"),
             };
-            let layer_id = self.repo.write_stream(splitstream, None)?;
 
-            // We intentionally explicitly ignore this, even though we're supposed to check it.
-            // See https://github.com/containers/containers-image-proxy-rs/issues/80 for discussion
-            // about why.  Note: we only care about the uncompressed layer tar, and we checksum it
-            // ourselves.
-            drop(driver);
+            // skopeo is doing data checksums for us to make sure the content we received is equal
+            // to the claimed diff_id. We trust it, but we need to check it by awaiting the driver.
+            driver.await?;
 
-            Ok(layer_id)
+            // Now we know that the content is what we expected.  Write it.
+            self.repo.write_stream(splitstream, &content_id, None)
         }
     }
 
@@ -142,20 +138,20 @@ impl<ObjectID: FsVerityHashValue> ImageOp<ObjectID> {
         manifest_layers: &[Descriptor],
         descriptor: &Descriptor,
     ) -> Result<ContentAndVerity<ObjectID>> {
-        let config_sha256 = sha256_from_descriptor(descriptor)?;
-        if let Some(config_id) = self.repo.check_stream(&config_sha256)? {
+        let config_digest: &str = descriptor.digest().as_ref();
+        let content_id = config_identifier(config_digest);
+
+        if let Some(config_id) = self.repo.has_stream(&content_id)? {
             // We already got this config?  Nice.
-            self.progress.println(format!(
-                "Already have container config {}",
-                hex::encode(config_sha256)
-            ))?;
-            Ok((config_sha256, config_id))
+            self.progress
+                .println(format!("Already have container config {config_digest}"))?;
+            Ok((config_digest.to_string(), config_id))
         } else {
             // We need to add the config to the repo.  We need to parse the config and make sure we
             // have all of the layers first.
             //
             self.progress
-                .println(format!("Fetching config {}", hex::encode(config_sha256)))?;
+                .println(format!("Fetching config {config_digest}"))?;
 
             let (mut config, driver) = self.proxy.get_descriptor(&self.img, descriptor).await?;
             let config = async move {
@@ -178,30 +174,29 @@ impl<ObjectID: FsVerityHashValue> ImageOp<ObjectID> {
             let sem = Arc::new(Semaphore::new(threads.into()));
             let mut entries = vec![];
             for (mld, diff_id) in layers {
+                let diff_id_ = diff_id.clone();
                 let self_ = Arc::clone(self);
                 let permit = Arc::clone(&sem).acquire_owned().await?;
-                let layer_sha256 = sha256_from_digest(diff_id)?;
                 let descriptor = mld.clone();
                 let future = tokio::spawn(async move {
                     let _permit = permit;
-                    self_.ensure_layer(layer_sha256, &descriptor).await
+                    self_.ensure_layer(&diff_id_, &descriptor).await
                 });
-                entries.push((layer_sha256, future));
+                entries.push((diff_id, future));
             }
+
+            let mut splitstream = self.repo.create_stream(OCI_CONFIG_CONTENT_TYPE);
 
             // Collect the results.
-            let mut config_maps = DigestMap::new();
-            for (layer_sha256, future) in entries {
-                config_maps.insert(&layer_sha256, &future.await??);
+            for (diff_id, future) in entries {
+                splitstream.add_named_stream_ref(diff_id, &future.await??);
             }
 
-            let mut splitstream = self
-                .repo
-                .create_stream(Some(config_sha256), Some(config_maps));
+            // NB: We trust that skopeo has verified that raw_config has the correct digest
             splitstream.write_inline(&raw_config);
-            let config_id = self.repo.write_stream(splitstream, None)?;
 
-            Ok((config_sha256, config_id))
+            let config_id = self.repo.write_stream(splitstream, &content_id, None)?;
+            Ok((content_id, config_id))
         }
     }
 
@@ -230,7 +225,7 @@ pub async fn pull<ObjectID: FsVerityHashValue>(
     imgref: &str,
     reference: Option<&str>,
     img_proxy_config: Option<ImageProxyConfig>,
-) -> Result<(Sha256Digest, ObjectID)> {
+) -> Result<(String, ObjectID)> {
     let op = Arc::new(ImageOp::new(repo, imgref, img_proxy_config).await?);
     let (sha256, id) = op
         .pull()
@@ -238,7 +233,7 @@ pub async fn pull<ObjectID: FsVerityHashValue>(
         .with_context(|| format!("Unable to pull container image {imgref}"))?;
 
     if let Some(name) = reference {
-        repo.name_stream(sha256, name)?;
+        repo.name_stream(&sha256, name)?;
     }
     Ok((sha256, id))
 }

crates/cfsctl/src/main.rs
@@ -48,7 +48,7 @@ pub struct App {
 enum OciCommand {
     /// Stores a tar file as a splitstream in the repository.
     ImportLayer {
-        sha256: String,
+        digest: String,
         name: Option<String>,
     },
     /// Lists the contents of a tar stream
@@ -105,7 +105,7 @@ enum Command {
     Transaction,
     /// Reconstitutes a split stream and writes it to stdout
     Cat {
-        /// the name of the stream to cat, either a sha256 digest or prefixed with 'ref/'
+        /// the name of the stream to cat, either a content identifier or prefixed with 'ref/'
         name: String,
     },
     /// Perform garbage collection
@@ -122,7 +122,7 @@ enum Command {
     },
     /// Mounts a composefs, possibly enforcing fsverity of the image
     Mount {
-        /// the name of the image to mount, either a sha256 digest or prefixed with 'ref/'
+        /// the name of the image to mount, either an fs-verity hash or prefixed with 'ref/'
         name: String,
         /// the mountpoint
         mountpoint: String,
@@ -194,7 +194,7 @@ async fn main() -> Result<()> {
             }
         }
         Command::Cat { name } => {
-            repo.merge_splitstream(&name, None, &mut std::io::stdout())?;
+            repo.merge_splitstream(&name, None, None, &mut std::io::stdout())?;
         }
         Command::ImportImage { reference } => {
             let image_id = repo.import_image(&reference, &mut std::io::stdin())?;
@@ -202,10 +202,10 @@ async fn main() -> Result<()> {
         }
         #[cfg(feature = "oci")]
         Command::Oci { cmd: oci_cmd } => match oci_cmd {
-            OciCommand::ImportLayer { name, sha256 } => {
+            OciCommand::ImportLayer { name, digest } => {
                 let object_id = composefs_oci::import_layer(
                     &Arc::new(repo),
-                    &composefs::util::parse_sha256(sha256)?,
+                    &digest,
                     name.as_deref(),
                     &mut std::io::stdin(),
                 )?;
@@ -253,10 +253,10 @@ async fn main() -> Result<()> {
                 println!("{}", image_id.to_id());
             }
             OciCommand::Pull { ref image, name } => {
-                let (sha256, verity) =
+                let (digest, verity) =
                     composefs_oci::pull(&Arc::new(repo), image, name.as_deref(), None).await?;
 
-                println!("sha256 {}", hex::encode(sha256));
+                println!("config {digest}");
                 println!("verity {}", verity.to_hex());
             }
             OciCommand::Seal {
@@ -264,9 +264,9 @@ async fn main() -> Result<()> {
                 ref config_verity,
             } => {
                 let verity = verity_opt(config_verity)?;
-                let (sha256, verity) =
+                let (digest, verity) =
                     composefs_oci::seal(&Arc::new(repo), config_name, verity.as_ref())?;
-                println!("sha256 {}", hex::encode(sha256));
+                println!("config {digest}");
                 println!("verity {}", verity.to_id());
             }
             OciCommand::Mount {
@@ -367,8 +367,8 @@ async fn main() -> Result<()> {
         }
         #[cfg(feature = "http")]
         Command::Fetch { url, name } => {
-            let (sha256, verity) = composefs_http::download(&url, &name, Arc::new(repo)).await?;
-            println!("sha256 {}", hex::encode(sha256));
+            let (digest, verity) = composefs_http::download(&url, &name, Arc::new(repo)).await?;
+            println!("content {digest}");
             println!("verity {}", verity.to_hex());
         }
     }

crates/composefs-http/src/lib.rs
@@ -7,7 +7,6 @@
 use std::{
     collections::{HashMap, HashSet},
     fs::File,
-    io::Read,
     sync::Arc,
 };
 
@@ -19,10 +18,7 @@ use sha2::{Digest, Sha256};
 use tokio::task::JoinSet;
 
 use composefs::{
-    fsverity::FsVerityHashValue,
-    repository::Repository,
-    splitstream::{DigestMapEntry, SplitStreamReader},
-    util::Sha256Digest,
+    fsverity::FsVerityHashValue, repository::Repository, splitstream::SplitStreamReader,
 };
 
 struct Downloader<ObjectID: FsVerityHashValue> {
@@ -66,17 +62,11 @@ impl<ObjectID: FsVerityHashValue> Downloader<ObjectID> {
         }
     }
 
-    fn open_splitstream(&self, id: &ObjectID) -> Result<SplitStreamReader<File, ObjectID>> {
-        SplitStreamReader::new(File::from(self.repo.open_object(id)?))
+    fn open_splitstream(&self, id: &ObjectID) -> Result<SplitStreamReader<ObjectID>> {
+        SplitStreamReader::new(File::from(self.repo.open_object(id)?), None)
     }
 
-    fn read_object(&self, id: &ObjectID) -> Result<Vec<u8>> {
-        let mut data = vec![];
-        File::from(self.repo.open_object(id)?).read_to_end(&mut data)?;
-        Ok(data)
-    }
-
-    async fn ensure_stream(self: &Arc<Self>, name: &str) -> Result<(Sha256Digest, ObjectID)> {
+    async fn ensure_stream(self: &Arc<Self>, name: &str) -> Result<(String, ObjectID)> {
         let progress = ProgressBar::new(2); // the first object gets "ensured" twice
         progress.set_style(
             ProgressStyle::with_template(
@@ -113,8 +103,8 @@ impl<ObjectID: FsVerityHashValue> Downloader<ObjectID> {
 
             // this part is fast: it only touches the header
             let mut reader = self.open_splitstream(&id)?;
-            for DigestMapEntry { verity, body } in &reader.refs.map {
-                match splitstreams.insert(verity.clone(), Some(*body)) {
+            for (body, verity) in reader.iter_named_refs() {
+                match splitstreams.insert(verity.clone(), Some(body.to_string())) {
                     // This is the (normal) case if we encounter a splitstream we didn't see yet...
                     None => {
                         splitstreams_todo.push(verity.clone());
@@ -125,7 +115,7 @@ impl<ObjectID: FsVerityHashValue> Downloader<ObjectID> {
                     // verify the SHA-256 content hashes later (after we get all the objects) so we
                     // need to make sure that all referents of this stream agree on what that is.
                     Some(Some(previous)) => {
-                        if previous != *body {
+                        if previous != body {
                             bail!(
                                 "Splitstream with verity {verity:?} has different body hashes {} and {}",
                                 hex::encode(previous),
@@ -208,8 +198,8 @@ impl<ObjectID: FsVerityHashValue> Downloader<ObjectID> {
         for (id, expected_checksum) in splitstreams {
             let mut reader = self.open_splitstream(&id)?;
             let mut context = Sha256::new();
-            reader.cat(&mut context, |id| self.read_object(id))?;
-            let measured_checksum: Sha256Digest = context.finalize().into();
+            reader.cat(&self.repo, &mut context)?;
+            let measured_checksum = format!("sha256:{}", hex::encode(context.finalize()));
 
             if let Some(expected) = expected_checksum {
                 if measured_checksum != expected {
@@ -265,7 +255,7 @@ pub async fn download<ObjectID: FsVerityHashValue>(
     url: &str,
     name: &str,
     repo: Arc<Repository<ObjectID>>,
-) -> Result<(Sha256Digest, ObjectID)> {
+) -> Result<(String, ObjectID)> {
     let downloader = Arc::new(Downloader {
         client: Client::new(),
         repo,

crates/composefs-oci/src/image.rs
@@ -11,7 +11,7 @@
 use std::{ffi::OsStr, os::unix::ffi::OsStrExt, rc::Rc};
 
 use anyhow::{ensure, Context, Result};
-use oci_spec::image::ImageConfiguration;
+use sha2::{Digest, Sha256};
 
 use composefs::{
     fsverity::FsVerityHashValue,
@@ -19,6 +19,7 @@ use composefs::{
     tree::{Directory, FileSystem, Inode, Leaf},
 };
 
+use crate::skopeo::TAR_LAYER_CONTENT_TYPE;
 use crate::tar::{TarEntry, TarItem};
 
 /// Processes a single tar entry and adds it to the filesystem.
@@ -84,6 +85,10 @@ pub fn process_entry<ObjectID: FsVerityHashValue>(
 
 /// Creates a filesystem from the given OCI container.  No special transformations are performed to
 /// make the filesystem bootable.
+///
+/// If `config_verity` is given it is used to get the OCI config splitstream by its fs-verity ID
+/// and the entire process is substantially faster.  If it is not given, the config and layers will
+/// be hashed to ensure that they match their claimed blob IDs.
 pub fn create_filesystem<ObjectID: FsVerityHashValue>(
     repo: &Repository<ObjectID>,
     config_name: &str,
@@ -91,14 +96,27 @@ pub fn create_filesystem<ObjectID: FsVerityHashValue>(
 ) -> Result<FileSystem<ObjectID>> {
     let mut filesystem = FileSystem::default();
 
-    let mut config_stream = repo.open_stream(config_name, config_verity)?;
-    let config = ImageConfiguration::from_reader(&mut config_stream)?;
+    let (config, map) = crate::open_config(repo, config_name, config_verity)?;
 
     for diff_id in config.rootfs().diff_ids() {
-        let layer_sha256 = super::sha256_from_digest(diff_id)?;
-        let layer_verity = config_stream.lookup(&layer_sha256)?;
+        let layer_verity = map
+            .get(diff_id.as_str())
+            .context("OCI config splitstream missing named ref to layer {diff_id}")?;
 
-        let mut layer_stream = repo.open_stream(&hex::encode(layer_sha256), Some(layer_verity))?;
+        if config_verity.is_none() {
+            // We don't have any proof that the named references in the config splitstream are
+            // trustworthy. We have no choice but to perform expensive validation of the layer
+            // stream.
+            let mut layer_stream =
+                repo.open_stream("", Some(layer_verity), Some(TAR_LAYER_CONTENT_TYPE))?;
+            let mut context = Sha256::new();
+            layer_stream.cat(repo, &mut context)?;
+            let content_hash = format!("sha256:{}", hex::encode(context.finalize()));
+            ensure!(content_hash == *diff_id, "Layer has incorrect checksum");
+        }
+
+        let mut layer_stream =
+            repo.open_stream("", Some(layer_verity), Some(TAR_LAYER_CONTENT_TYPE))?;
         while let Some(entry) = crate::tar::get_entry(&mut layer_stream)? {
             process_entry(&mut filesystem, entry)?;
         }

commit ad1faa20e18d3ec4fa7d11b8a78bde1c7c5a7784

skopeo: Fix layer reference ordering in config splitstream
Add layer references in the original manifest diff_id order rather
than the order layers finish downloading. This ensures reproducible
config splitstream digests regardless of download timing.

Fixes: 54c3c6de ("splitstream: Rework file format")
Assisted-by: OpenCode (Claude claude-opus-4-5-20250514)

crates/composefs-oci/src/skopeo.rs
@@ -247,9 +247,18 @@ impl<ObjectID: FsVerityHashValue> ImageOp<ObjectID> {
 
             let mut splitstream = self.repo.create_stream(OCI_CONFIG_CONTENT_TYPE);
 
-            // Collect the results.
+            // Collect the results and build a map of diff_id -> verity
+            let mut layer_verities = std::collections::HashMap::new();
             for (diff_id, future) in entries {
-                splitstream.add_named_stream_ref(diff_id, &future.await??);
+                layer_verities.insert(diff_id.clone(), future.await??);
+            }
+
+            // Add layer references in the original diff_id order (not download order)
+            for diff_id in config.rootfs().diff_ids() {
+                let verity = layer_verities
+                    .get(diff_id)
+                    .ok_or_else(|| anyhow::anyhow!("Missing verity for layer {diff_id}"))?;
+                splitstream.add_named_stream_ref(diff_id, verity);
             }
 
             // NB: We trust that skopeo has verified that raw_config has the correct digest